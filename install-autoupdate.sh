614de47f47b4729cf2961b459e7badd453c1e656
Michael Taylor <mtaylor@michaeltaylor.dev>
2024-09-27 12:41:48 -0400

    feat: add auto update cron script

---
+++ install-autoupdate.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Adds a cron job to run the upgrade-node script once a day
+
+source ./lib/setup_auto_update.sh
+
+# Ask if the user wants to set up automatic updates
+setup_auto_update
